07827ea02ac447f429ab4822d22fdb8815e7b93a
Arne Georg Gleditsch <arne.gleditsch@numascale.com>
2011-10-20 15:57:37 +0200

    Truncate git show to first line, corrects problem with auto-git-log.

--- bootloader/gen-gitlog.sh
+++ bootloader/gen-gitlog.sh
@@ -5,13 +5,13 @@ PREFIX=$2
 
 exec >&2
 
-SHA=$(git show --pretty=format:'%H' HEAD)
+SHA=$(git show --pretty=format:'%H' HEAD | head -1)
 TEMP=$(mktemp -t $TARGET.XXXXXXXXXX)
 
-echo "char *${PREFIX}_sha = \"$SHA\";" > $TEMP
-echo -n "char *${PREFIX}_diff = \"" >> $TEMP
-git diff $SHA | perl -npe 's/([\\\"])/\\\1/g; s/\n/\\n/g; s/\t/\\t/g;' >> $TEMP
-echo "\";" >> $TEMP
+(echo "char *${PREFIX}_sha = \"$SHA\";";
+echo -n "char *${PREFIX}_diff = \"";
+git diff $SHA | perl -npe 's/([\\\"])/\\\1/g; s/\n/\\n/g; s/\t/\\t/g;';
+echo "\";") > $TEMP
 
 diff -Nq $TEMP $TARGET > /dev/null || mv $TEMP $TARGET
 rm -f $TEMP
