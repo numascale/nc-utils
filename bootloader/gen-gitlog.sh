9ee7abae6dba6446eb7d7f3d2503d061a23c7744
Daniel J Blueman <daniel@numascale-asia.com>
2012-04-12 11:01:00 +0800

    Add include line for external declarations

--- bootloader/gen-gitlog.sh
+++ bootloader/gen-gitlog.sh
@@ -8,7 +8,8 @@ exec >&2
 SHA=$(git show --pretty=format:'%H' HEAD | head -1)
 TEMP=$(mktemp -t $TARGET.XXXXXXXXXX)
 
-(echo "char *${PREFIX}_sha = \"$SHA\";";
+(echo "#include \"auto-dnc-gitlog.h\"";
+echo "char *${PREFIX}_sha = \"$SHA\";";
 echo -n "char *${PREFIX}_diff = \"";
 git diff $SHA | perl -npe 's/([\\\"])/\\\1/g; s/\n/\\n/g; s/\t/\\t/g;';
 echo "\";") > $TEMP
