ad2deb7c2db527e15a457e685d5ea8aa8246c979
Atle Vesterkjær <av@numascale.com>
2012-07-05 14:22:25 +0200

    Added new gui expansion without mpi integration in nc-stat3

---
+++ os/nc-gui/nc-stat3/GeneratedFiles/Debug/moc_mpistat.cpp
@@ -0,0 +1,496 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mpistat.hpp'
+**
+** Created: Thu 5. Jul 13:19:34 2012
+**      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../mpistat.hpp"
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mpistat.hpp' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 63
+#error "This file was generated using the moc from 4.8.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+static const uint qt_meta_data_mpistat[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: signature, parameters, type, tag, flags
+       9,    8,    8,    8, 0x08,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_mpistat[] = {
+    "mpistat\0\0getinfo()\0"
+};
+
+void mpistat::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        mpistat *_t = static_cast<mpistat *>(_o);
+        switch (_id) {
+        case 0: _t->getinfo(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObjectExtraData mpistat::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject mpistat::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_mpistat,
+      qt_meta_data_mpistat, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &mpistat::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *mpistat::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *mpistat::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_mpistat))
+        return static_cast<void*>(const_cast< mpistat*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int mpistat::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    }
+    return _id;
+}
+static const uint qt_meta_data_HistGraph[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: signature, parameters, type, tag, flags
+      15,   11,   10,   10, 0x0a,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_HistGraph[] = {
+    "HistGraph\0\0,on\0showCurve(QwtPlotItem*,bool)\0"
+};
+
+void HistGraph::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        HistGraph *_t = static_cast<HistGraph *>(_o);
+        switch (_id) {
+        case 0: _t->showCurve((*reinterpret_cast< QwtPlotItem*(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObjectExtraData HistGraph::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject HistGraph::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_HistGraph,
+      qt_meta_data_HistGraph, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &HistGraph::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *HistGraph::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *HistGraph::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_HistGraph))
+        return static_cast<void*>(const_cast< HistGraph*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int HistGraph::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    }
+    return _id;
+}
+static const uint qt_meta_data_CacheGraph[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: signature, parameters, type, tag, flags
+      16,   12,   11,   11, 0x0a,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_CacheGraph[] = {
+    "CacheGraph\0\0,on\0showCurve(QwtPlotItem*,bool)\0"
+};
+
+void CacheGraph::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        CacheGraph *_t = static_cast<CacheGraph *>(_o);
+        switch (_id) {
+        case 0: _t->showCurve((*reinterpret_cast< QwtPlotItem*(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObjectExtraData CacheGraph::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject CacheGraph::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_CacheGraph,
+      qt_meta_data_CacheGraph, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &CacheGraph::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *CacheGraph::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *CacheGraph::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_CacheGraph))
+        return static_cast<void*>(const_cast< CacheGraph*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int CacheGraph::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    }
+    return _id;
+}
+static const uint qt_meta_data_CacheHistGraph[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: signature, parameters, type, tag, flags
+      20,   16,   15,   15, 0x0a,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_CacheHistGraph[] = {
+    "CacheHistGraph\0\0,on\0showCurve(QwtPlotItem*,bool)\0"
+};
+
+void CacheHistGraph::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        CacheHistGraph *_t = static_cast<CacheHistGraph *>(_o);
+        switch (_id) {
+        case 0: _t->showCurve((*reinterpret_cast< QwtPlotItem*(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObjectExtraData CacheHistGraph::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject CacheHistGraph::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_CacheHistGraph,
+      qt_meta_data_CacheHistGraph, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &CacheHistGraph::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *CacheHistGraph::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *CacheHistGraph::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_CacheHistGraph))
+        return static_cast<void*>(const_cast< CacheHistGraph*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int CacheHistGraph::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    }
+    return _id;
+}
+static const uint qt_meta_data_SizeHistGraph[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_SizeHistGraph[] = {
+    "SizeHistGraph\0"
+};
+
+void SizeHistGraph::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
+}
+
+const QMetaObjectExtraData SizeHistGraph::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject SizeHistGraph::staticMetaObject = {
+    { &HistGraph::staticMetaObject, qt_meta_stringdata_SizeHistGraph,
+      qt_meta_data_SizeHistGraph, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &SizeHistGraph::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *SizeHistGraph::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *SizeHistGraph::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_SizeHistGraph))
+        return static_cast<void*>(const_cast< SizeHistGraph*>(this));
+    return HistGraph::qt_metacast(_clname);
+}
+
+int SizeHistGraph::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = HistGraph::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    return _id;
+}
+static const uint qt_meta_data_SendLatencyGraph[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_SendLatencyGraph[] = {
+    "SendLatencyGraph\0"
+};
+
+void SendLatencyGraph::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
+}
+
+const QMetaObjectExtraData SendLatencyGraph::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject SendLatencyGraph::staticMetaObject = {
+    { &HistGraph::staticMetaObject, qt_meta_stringdata_SendLatencyGraph,
+      qt_meta_data_SendLatencyGraph, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &SendLatencyGraph::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *SendLatencyGraph::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *SendLatencyGraph::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_SendLatencyGraph))
+        return static_cast<void*>(const_cast< SendLatencyGraph*>(this));
+    return HistGraph::qt_metacast(_clname);
+}
+
+int SendLatencyGraph::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = HistGraph::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    return _id;
+}
+static const uint qt_meta_data_BandwidthGraph[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_BandwidthGraph[] = {
+    "BandwidthGraph\0"
+};
+
+void BandwidthGraph::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
+}
+
+const QMetaObjectExtraData BandwidthGraph::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject BandwidthGraph::staticMetaObject = {
+    { &HistGraph::staticMetaObject, qt_meta_stringdata_BandwidthGraph,
+      qt_meta_data_BandwidthGraph, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &BandwidthGraph::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *BandwidthGraph::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *BandwidthGraph::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_BandwidthGraph))
+        return static_cast<void*>(const_cast< BandwidthGraph*>(this));
+    return HistGraph::qt_metacast(_clname);
+}
+
+int BandwidthGraph::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = HistGraph::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    return _id;
+}
+QT_END_MOC_NAMESPACE
---
+++ os/nc-gui/nc-stat3/GeneratedFiles/ui_mpistat.h
@@ -0,0 +1,210 @@
+/********************************************************************************
+** Form generated from reading UI file 'mpistat.ui'
+**
+** Created: Thu 5. Jul 13:19:34 2012
+**      by: Qt User Interface Compiler version 4.8.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MPISTAT_H
+#define UI_MPISTAT_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QMainWindow>
+#include <QtGui/QPushButton>
+#include <QtGui/QSpacerItem>
+#include <QtGui/QTabWidget>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_mpistatClass
+{
+public:
+    QWidget *centralWidget;
+    QVBoxLayout *verticalLayout_2;
+    QVBoxLayout *verticalLayout;
+    QSpacerItem *verticalSpacer_2;
+    QTabWidget *tabWidget;
+    QWidget *tab5;
+    QVBoxLayout *verticalLayout_7;
+    QVBoxLayout *tab5layout;
+    QWidget *tab1;
+    QVBoxLayout *verticalLayout_4;
+    QVBoxLayout *tab1layout;
+    QWidget *tab2;
+    QVBoxLayout *verticalLayout_6;
+    QVBoxLayout *tab2layout;
+    QWidget *tab3;
+    QVBoxLayout *verticalLayout_5;
+    QVBoxLayout *tab3layout;
+    QWidget *tab4;
+    QVBoxLayout *verticalLayout_8;
+    QVBoxLayout *tab4layout;
+    QSpacerItem *verticalSpacer_3;
+    QWidget *widget;
+    QHBoxLayout *horizontalLayout_2;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *pushButton;
+    QSpacerItem *verticalSpacer;
+
+    void setupUi(QMainWindow *mpistatClass)
+    {
+        if (mpistatClass->objectName().isEmpty())
+            mpistatClass->setObjectName(QString::fromUtf8("mpistatClass"));
+        mpistatClass->resize(919, 595);
+        mpistatClass->setMinimumSize(QSize(100, 160));
+        centralWidget = new QWidget(mpistatClass);
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        verticalLayout_2 = new QVBoxLayout(centralWidget);
+        verticalLayout_2->setSpacing(6);
+        verticalLayout_2->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setSpacing(6);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalSpacer_2 = new QSpacerItem(20, 10, QSizePolicy::Minimum, QSizePolicy::Fixed);
+
+        verticalLayout->addItem(verticalSpacer_2);
+
+        tabWidget = new QTabWidget(centralWidget);
+        tabWidget->setObjectName(QString::fromUtf8("tabWidget"));
+        tabWidget->setMovable(true);
+        tab5 = new QWidget();
+        tab5->setObjectName(QString::fromUtf8("tab5"));
+        verticalLayout_7 = new QVBoxLayout(tab5);
+        verticalLayout_7->setSpacing(6);
+        verticalLayout_7->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_7->setObjectName(QString::fromUtf8("verticalLayout_7"));
+        tab5layout = new QVBoxLayout();
+        tab5layout->setSpacing(6);
+        tab5layout->setObjectName(QString::fromUtf8("tab5layout"));
+
+        verticalLayout_7->addLayout(tab5layout);
+
+        tabWidget->addTab(tab5, QString());
+        tab1 = new QWidget();
+        tab1->setObjectName(QString::fromUtf8("tab1"));
+        verticalLayout_4 = new QVBoxLayout(tab1);
+        verticalLayout_4->setSpacing(6);
+        verticalLayout_4->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_4->setObjectName(QString::fromUtf8("verticalLayout_4"));
+        tab1layout = new QVBoxLayout();
+        tab1layout->setSpacing(6);
+        tab1layout->setObjectName(QString::fromUtf8("tab1layout"));
+
+        verticalLayout_4->addLayout(tab1layout);
+
+        tabWidget->addTab(tab1, QString());
+        tab2 = new QWidget();
+        tab2->setObjectName(QString::fromUtf8("tab2"));
+        verticalLayout_6 = new QVBoxLayout(tab2);
+        verticalLayout_6->setSpacing(6);
+        verticalLayout_6->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_6->setObjectName(QString::fromUtf8("verticalLayout_6"));
+        tab2layout = new QVBoxLayout();
+        tab2layout->setSpacing(6);
+        tab2layout->setObjectName(QString::fromUtf8("tab2layout"));
+
+        verticalLayout_6->addLayout(tab2layout);
+
+        tabWidget->addTab(tab2, QString());
+        tab3 = new QWidget();
+        tab3->setObjectName(QString::fromUtf8("tab3"));
+        verticalLayout_5 = new QVBoxLayout(tab3);
+        verticalLayout_5->setSpacing(6);
+        verticalLayout_5->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_5->setObjectName(QString::fromUtf8("verticalLayout_5"));
+        tab3layout = new QVBoxLayout();
+        tab3layout->setSpacing(6);
+        tab3layout->setObjectName(QString::fromUtf8("tab3layout"));
+
+        verticalLayout_5->addLayout(tab3layout);
+
+        tabWidget->addTab(tab3, QString());
+        tab4 = new QWidget();
+        tab4->setObjectName(QString::fromUtf8("tab4"));
+        verticalLayout_8 = new QVBoxLayout(tab4);
+        verticalLayout_8->setSpacing(6);
+        verticalLayout_8->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_8->setObjectName(QString::fromUtf8("verticalLayout_8"));
+        tab4layout = new QVBoxLayout();
+        tab4layout->setSpacing(6);
+        tab4layout->setObjectName(QString::fromUtf8("tab4layout"));
+
+        verticalLayout_8->addLayout(tab4layout);
+
+        tabWidget->addTab(tab4, QString());
+
+        verticalLayout->addWidget(tabWidget);
+
+        verticalSpacer_3 = new QSpacerItem(20, 10, QSizePolicy::Minimum, QSizePolicy::Fixed);
+
+        verticalLayout->addItem(verticalSpacer_3);
+
+        widget = new QWidget(centralWidget);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setMinimumSize(QSize(0, 0));
+        horizontalLayout_2 = new QHBoxLayout(widget);
+        horizontalLayout_2->setSpacing(6);
+        horizontalLayout_2->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        pushButton = new QPushButton(widget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setMaximumSize(QSize(80, 16777215));
+
+        horizontalLayout->addWidget(pushButton);
+
+
+        horizontalLayout_2->addLayout(horizontalLayout);
+
+
+        verticalLayout->addWidget(widget);
+
+        verticalSpacer = new QSpacerItem(20, 10, QSizePolicy::Minimum, QSizePolicy::Fixed);
+
+        verticalLayout->addItem(verticalSpacer);
+
+
+        verticalLayout_2->addLayout(verticalLayout);
+
+        mpistatClass->setCentralWidget(centralWidget);
+
+        retranslateUi(mpistatClass);
+
+        tabWidget->setCurrentIndex(1);
+
+
+        QMetaObject::connectSlotsByName(mpistatClass);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *mpistatClass)
+    {
+        mpistatClass->setWindowTitle(QApplication::translate("mpistatClass", "mpistat", 0, QApplication::UnicodeUTF8));
+        tabWidget->setTabText(tabWidget->indexOf(tab5), QApplication::translate("mpistatClass", "Cache rate", 0, QApplication::UnicodeUTF8));
+        tabWidget->setTabText(tabWidget->indexOf(tab1), QApplication::translate("mpistatClass", "Cache rate distribution", 0, QApplication::UnicodeUTF8));
+        tabWidget->setTabText(tabWidget->indexOf(tab2), QApplication::translate("mpistatClass", "Send latency", 0, QApplication::UnicodeUTF8));
+        tabWidget->setTabText(tabWidget->indexOf(tab3), QApplication::translate("mpistatClass", "Receive latency", 0, QApplication::UnicodeUTF8));
+        tabWidget->setTabText(tabWidget->indexOf(tab4), QApplication::translate("mpistatClass", "Bandwidth", 0, QApplication::UnicodeUTF8));
+        pushButton->setText(QApplication::translate("mpistatClass", "PDF", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class mpistatClass: public Ui_mpistatClass {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MPISTAT_H
