f3c2773e35d37ebc2ccddb555cb3c00cc9f63006
av <av@numscale.com>
2012-08-26 12:38:31 +0200

    Renamed files to names easier to underestand

--- os/nc-gui/nc-pstat-gui/GeneratedFiles/Debug/moc_nc-stat.cpp
+++ os/nc-gui/nc-pstat-gui/GeneratedFiles/Debug/moc_nc-stat.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'nc-stat.hpp'
 **
-** Created: Thu 23. Aug 16:07:05 2012
+** Created: Sun 26. Aug 12:35:13 2012
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
 **
 ** WARNING! All changes made in this file will be lost!
