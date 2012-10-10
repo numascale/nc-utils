4253928280f76a53bd1bcbebd2b3d20edf9e05a5
Daniel J Blueman <daniel@quora.org>
2012-10-10 15:04:43 +0800

    Handoff ACPI first, as Linux does

--- bootloader/dnc-devices.c
+++ bootloader/dnc-devices.c
@@ -323,6 +323,9 @@ void handover_legacy(void)
     if (disable_smm)
 	return;
 
+    /* Stop ACPI first, as Linux requests ownership of this before other subsystems */
+    stop_acpi();
+
     const struct devspec devices[] = {
 	{PCI_CLASS_SERIAL_USB_OHCI, 3, stop_ohci},
 	{PCI_CLASS_SERIAL_USB_EHCI, 3, stop_ehci},
@@ -333,6 +336,5 @@ void handover_legacy(void)
     };
 
     pci_search(devices);
-    stop_acpi();
 }
 
