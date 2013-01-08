f12a59716c97e63fd82cf60e77d2604ac7dd3825
Daniel J Blueman <daniel@numascale-asia.com>
2013-01-08 16:47:13 +0800

    Make phy self-test iterations easier to read

--- bootloader/dnc-commonlib.c
+++ bootloader/dnc-commonlib.c
@@ -2002,7 +2002,7 @@ static int perform_selftest(int asic_mode, char p_type[16])
 	/* Trigger a HSS PLL reset */
 	_pic_reset_ctrl(1);
 
-	printf("HSS");
+	printf("HSS ");
 	for (int phy = 0; phy < 6; phy++) {
 	    /* 1. Check that the HSS PLL has locked */
 	    val = dnc_read_csr(0xfff0, H2S_CSR_G0_HSSXA_STAT_1 + 0x40 * phy);
@@ -2067,7 +2067,7 @@ static int perform_selftest(int asic_mode, char p_type[16])
 	    dnc_write_csr(0xfff0, H2S_CSR_G0_HSSXA_CTR_1 + 0x40 * phy, val & ~(0xf));
 	    dnc_write_csr(0xfff0, H2S_CSR_G0_HSSXA_CTR_8 + 0x40 * phy, 0x00f0);
 	    dnc_write_csr(0xfff0, H2S_CSR_G0_HSSXA_CTR_8 + 0x40 * phy, 0x0000);
-	    printf(_get_linkname(phy));
+	    printf("%s ", _get_linkname(phy));
 	}
 	/* Trigger a HSS PLL reset */
 	_pic_reset_ctrl(1);
