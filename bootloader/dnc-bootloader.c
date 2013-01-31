6cb7bd2eb16b23772f1b544539bbf0435046a621
Daniel J Blueman <daniel@numascale-asia.com>
2013-01-31 11:11:34 +0800

    Cleary memory in parallel after unification to prevent delays

--- bootloader/dnc-bootloader.c
+++ bootloader/dnc-bootloader.c
@@ -1467,36 +1467,6 @@ static void setup_remote_cores(uint16_t num)
 	udelay(200);
 	*REL64(new_mcfg_msr) = DNC_MCFG_BASE | ((uint64_t)node << 28ULL) | 0x21ULL;
 
-	printf("Clearing SCI%03x memory...", node);
-	for (i = 0; i < 8; i++) {
-		if (!cur_node->ht[i].cpuid)
-			continue;
-
-		/* Disable memory controller prefetch */
-		val = dnc_read_conf(node, 0, 24 + i, FUNC2_DRAM, 0x11c);
-		dnc_write_conf(node, 0, 24 + i, FUNC2_DRAM, 0x11c, val | (3 << 12));
-
-		/* Start memory clearing */
-		val = dnc_read_conf(node, 0, 24 + i, FUNC2_DRAM, 0x110);
-		dnc_write_conf(node, 0, 24 + i, FUNC2_DRAM, 0x110, val | (1 << 3));
-	}
-
-	/* Wait for clear to finish */
-	for (i = 0; i < 8; i++) {
-		if (!cur_node->ht[i].cpuid)
-			continue;
-
-		do {
-			udelay(100);
-			val = dnc_read_conf(node, 0, 24 + i, FUNC2_DRAM, 0x110);
-		} while (val & (1 << 9));
-
-		/* Reenable memory controller prefetch */
-		val = dnc_read_conf(node, 0, 24 + i, FUNC2_DRAM, 0x11c);
-		dnc_write_conf(node, 0, 24 + i, FUNC2_DRAM, 0x11c, val & ~(3 << 12));
-	}
-	printf("done\n");
-
 	printf("APICs:");
 
 	/* Start all remote cores and let them run our init_trampoline */
@@ -2512,6 +2482,46 @@ static int unify_all_nodes(void)
 
 	for (i = 1; i < dnc_node_count; i++)
 		setup_remote_cores(i);
+
+	printf("Clearing remote memory...");
+	for (node = 1; node < dnc_node_count; node++) {
+		for (int ht = 0; ht < 8; ht++) {
+			if (!nc_node[node].ht[ht].cpuid)
+				continue;
+			uint16_t sci = nc_node[node].sci_id;
+			uint32_t val;
+
+			/* Disable memory controller prefetch */
+			val = dnc_read_conf(sci, 0, 24 + ht, FUNC2_DRAM, 0x11c);
+			dnc_write_conf(sci, 0, 24 + ht, FUNC2_DRAM, 0x11c, val | (3 << 12));
+
+			/* Start memory clearing */
+			val = dnc_read_conf(sci, 0, 24 + ht, FUNC2_DRAM, 0x110);
+			dnc_write_conf(sci, 0, 24 + ht, FUNC2_DRAM, 0x110, val | (1 << 3));
+		}
+	}
+
+	/* Wait for clear to finish */
+	for (node = 1; node < dnc_node_count; node++) {
+		for (int ht = 0; ht < 8; ht++) {
+			if (!nc_node[node].ht[ht].cpuid)
+				continue;
+			uint16_t sci = nc_node[node].sci_id;
+			uint32_t val;
+
+			while (1) {
+				val = dnc_read_conf(sci, 0, 24 + ht, FUNC2_DRAM, 0x110);
+				if (!(val & (1 << 9)))
+					break;
+				udelay(100);
+			}
+
+			/* Reenable memory controller prefetch */
+			val = dnc_read_conf(sci, 0, 24 + ht, FUNC2_DRAM, 0x11c);
+			dnc_write_conf(sci, 0, 24 + ht, FUNC2_DRAM, 0x11c, val & ~(3 << 12));
+		}
+	}
+	printf("done\n");
 
 	/* Re-enable DRAM scrubbers with our new memory map, required by fam15h BKDG; see D18F2xC0 b0 */
 	if (!trace_buf_size) {
