1cd96c542e4e830036d63953b8b6635dad477827
Daniel J Blueman <daniel@numascale-asia.com>
2013-03-26 10:44:07 +0800

    Add code to setup OEMN ACPI table

---
+++ bootloader/dnc-escrow.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2008-2012 Numascale AS, support@numascale.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "dnc-escrow.h"
+#include "dnc-config.h"
+#include "dnc-commonlib.h"
+
+#include <stdio.h>
+
+int escrow_populate(void *data)
+{
+	union escrow_ent *start = data;
+	union escrow_ent *cur = start;
+
+	cur->global.numachip_major = 1;
+	cur->global.numachip_minor = dnc_chip_rev;
+	cur->global.escrow_ver = 0;
+	cur->global.size_x = cfg_fabric.x_size;
+	cur->global.size_y = cfg_fabric.y_size;
+	cur->global.size_z = cfg_fabric.z_size;
+	cur->global.symmetric = 1;
+	cur->global.renumbering = renumber_bsp;
+	cur->global.neigh_ht = nc_neigh;
+	cur->global.neigh_link = nc_neigh_link;
+	cur++;
+	
+	return (cur - start) * sizeof(cur[0]);
+}
+
