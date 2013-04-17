6dc8da0672e569e1bcab68d072aa95d27e3063f0
Atle Vesterkjær <av@numascale.com>
2013-04-17 13:40:26 +0200

    Use ACPI tables instead of json file.

--- os/lib/init.c
+++ os/lib/init.c
@@ -15,7 +15,6 @@
 // Numascale AS Oslo, Norway.
 // All Rights Reserved.
 //
-
 #include <stdio.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -30,11 +29,55 @@
 
 #define NUMASCALE_VENDOR_ID   0x1B47
 #define NUMACHIP_DEVICE_ID    0x0601
-#define DEBUG_STATEMENT(x)
+#define DEBUG_STATEMENT(x) 
+
+struct acpi_sdt {
+	union {
+		char s[4];
+		uint32_t l;
+	} sig;
+	uint32_t len;
+	uint8_t revision;
+	uint8_t checksum;
+	unsigned char oemid[6];
+	unsigned char oemtableid[8];
+	uint32_t oemrev;
+	unsigned char creatorid[4];
+	uint32_t creatorrev;
+	unsigned char data[0];
+} __attribute__((packed));
+
+struct ent_global {
+	unsigned int numachip_rev : 4;
+	unsigned int size_x : 4;
+	unsigned int size_y : 4;
+	unsigned int size_z : 4;
+	unsigned int northbridges : 3;
+	unsigned int neigh_ht : 3;
+	unsigned int neigh_link : 2;
+	unsigned int symmetric : 1;
+	unsigned int renumbering : 1;
+	unsigned int remote_io : 1;
+} __attribute__((packed));
+
+struct ent_external {
+	uint8_t id;
+	uint8_t flags;
+	uint64_t base : 48;
+	uint16_t reserved;
+	uint64_t limit : 48;	
+} __attribute__((packed));
+
+union escrow_ent {
+	uint8_t id;
+	struct ent_global global;
+	struct ent_external external;
+} __attribute__((packed));
+
 static void add_device(int32_t nodeid,
-		       struct numachip_device ***dev_list,
-		       int32_t *ndevices,
-		       int32_t *list_size)
+					   struct numachip_device ***dev_list,
+					   int32_t *ndevices,
+					   int32_t *list_size)
 {
     struct numachip_device **new_list;
     struct numachip_device *dev;
@@ -66,17 +109,71 @@ HIDDEN int32_t numachip_init(struct numachip_device ***list, const char *filenam
     int32_t cfg_nodes;
     struct node_info *cfg_nodelist;
     int32_t i = 0;
+	
 
     //parse_json_file
     if (parse_config_file(filename,&cfg_nodelist,&cfg_nodes)) {
-	for (i = 0; i < cfg_nodes; i++) {
-	    DEBUG_STATEMENT(printf("Node %d: <%s> uuid: %d, sciid: 0x%03x, partition: %d, osc: %d, sync-only: %d\n",
-				   i, cfg_nodelist[i].desc, cfg_nodelist[i].uuid,
-				   cfg_nodelist[i].sciid, cfg_nodelist[i].partition,
-				   cfg_nodelist[i].osc, cfg_nodelist[i].sync_only));
-	    
-	    add_device(cfg_nodelist[i].sciid, list, &ndevices, &list_size);
-	}
+		for (i = 0; i < cfg_nodes; i++) {
+			DEBUG_STATEMENT(printf("Node %d: <%s> uuid: %d, sciid: 0x%03x, partition: %d, osc: %d, sync-only: %d\n",
+								   i, cfg_nodelist[i].desc, cfg_nodelist[i].uuid,
+								   cfg_nodelist[i].sciid, cfg_nodelist[i].partition,
+								   cfg_nodelist[i].osc, cfg_nodelist[i].sync_only));
+			
+			add_device(cfg_nodelist[i].sciid, list, &ndevices, &list_size);
+		}
+    }
+    return ndevices;
+}
+
+HIDDEN int32_t numachip_init_oem(struct numachip_device ***list)
+{
+    int32_t ndevices = 0;
+    int32_t list_size = 0;
+    int32_t sciid=0; 
+    int32_t x=0,y=0,z=0,i=0;
+    int32_t fd;
+    size_t len;
+    struct acpi_sdt *oemn;
+    union escrow_ent *cur;
+	
+    oemn = malloc(sizeof(struct acpi_sdt) + sizeof(union escrow_ent) + 1);
+    if (!oemn) {
+	    fprintf(stderr, "Unable to allocate %ld bytes of memory for OEMN table\n",
+	            sizeof(struct acpi_sdt) + sizeof(union escrow_ent) + 1);
+	    return -1;
+    }
+    
+    fd = open("/sys/firmware/acpi/tables/OEMN", O_RDONLY);
+    if (fd < 0) {
+	    fprintf(stderr, "Unable to open OEMN table: %d\n", fd);
+	    return -1;
+    }
+    
+    len = read(fd, oemn, sizeof(struct acpi_sdt) + sizeof(union escrow_ent));
+    if (len < 0) {
+	    fprintf(stderr, "Bad OEMN length: %ld\n", len);
+	    return -1;
+    }
+    
+    cur = (union escrow_ent *)oemn->data;
+    DEBUG_STATEMENT(printf("Escrow: numachip_rev=%d size=%d,%d,%d northbridges=%d neigh=%d,%d symmetric=%d renumbering=%d\n",
+						   cur->global.numachip_rev, cur->global.size_x, cur->global.size_y, cur->global.size_z,
+						   cur->global.northbridges, cur->global.neigh_ht, cur->global.neigh_link,
+						   cur->global.symmetric, cur->global.renumbering));
+    
+    
+    
+    for (z = 0; z < (cur->global.size_z+1); z++) {
+	    for (y = 0; y < (cur->global.size_y+1); y++) {
+		    for (x = 0; x < (cur->global.size_x); x++) {
+				
+			    sciid= (z<<8) + (y << 4) + x;
+			    DEBUG_STATEMENT(printf("Node %d: sciid: 0x%03x\n",
+									   i, sciid));
+			    i++;
+			    add_device(sciid, list, &ndevices, &list_size);
+		    }
+	    }
     }
     return ndevices;
 }
--- os/nc_test/nc_perf/false_params.sh
+++ os/nc_test/nc_perf/false_params.sh
@@ -1,62 +1,59 @@
 echo "BAD Clear all counters - node 0"
 echo "TESTING: ./nc_perf fabr"
 ./nc_perf fabr
-echo "TESTING:./nc_perf fabric-loop-05.json -counter"
-./nc_perf fabric-loop-05.json -counter
-echo "TESTING:./nc_perf fabric-loop-05.json -counter-clear 0 "
-./nc_perf fabric-loop-05.json -counter-clear 0 
-echo "TESTING:./nc_perf fabric-loop-05.json -counter-clear 0 3"
-./nc_perf fabric-loop-05.json -counter-clear 0 3
+echo "TESTING:./nc_perf  -counter"
+./nc_perf  -counter
+echo "TESTING:./nc_perf  -counter-clear 0 "
+./nc_perf -counter-clear 0 
+echo "TESTING:./nc_perf  -counter-clear 0 3"
+./nc_perf -counter-clear 0 3
 
 echo "Clear all counters - node 0"
-./nc_perf fabric-loop-05.json -counter-clear 0 0
-./nc_perf fabric-loop-05.json -counter-clear 0 1
-./nc_perf fabric-loop-05.json -counter-clear 0 2
-./nc_perf fabric-loop-05.json -counter-clear 0 3
+./nc_perf -counter-clear 0 0
+./nc_perf -counter-clear 0 1
+./nc_perf -counter-clear 0 2
+./nc_perf -counter-clear 0 3
 
 echo "BAD Select - only node 0" 
-echo "TESTING ./nc_perf fabric-loop-05.json -counter-select 0 "
-./nc_perf fabric-loop-05.json -counter-select 0 
-echo "TESTING ./nc_perf fabric-loop-05.json -counter-select 0 1"
-./nc_perf fabric-loop-05.json -counter-select 0 1
-echo "TESTING ./nc_perf fabric-loop-05.json -counter-select 0 9"
-./nc_perf fabric-loop-05.json -counter-select 0 1
-echo "TESTING ./nc_perf fabric-loop-05.json -counter-select 0 3 9"
-./nc_perf fabric-loop-05.json -counter-select 0 3 9
-
+echo "TESTING ./nc_perf  -counter-select 0 "
+./nc_perf -counter-select 0 
+echo "TESTING ./nc_perf  -counter-select 0 1"
+./nc_perf -counter-select 0 1
+echo "TESTING ./nc_perf  -counter-select 0 9"
+./nc_perf -counter-select 0 1
+echo "TESTING ./nc_perf  -counter-select 0 3 9"
+./nc_perf -counter-select 0 3 9
 
 echo "Select them - only node 0 - FIX" 
-./nc_perf fabric-loop-05.json -counter-clear 0 0
-./nc_perf fabric-loop-05.json -counter-clear 0 1
-./nc_perf fabric-loop-05.json -counter-clear 0 2
-./nc_perf fabric-loop-05.json -counter-clear 0 3
-./nc_perf fabric-loop-05.json -counter-select 0 0 1
-./nc_perf fabric-loop-05.json -counter-select 0 1 1
-./nc_perf fabric-loop-05.json -counter-select 0 2 6
-./nc_perf fabric-loop-05.json -counter-select 0 3 6
+./nc_perf -counter-clear 0 0
+./nc_perf -counter-clear 0 1
+./nc_perf -counter-clear 0 2
+./nc_perf -counter-clear 0 3
+./nc_perf -counter-select 0 0 1
+./nc_perf -counter-select 0 1 1
+./nc_perf -counter-select 0 2 6
+./nc_perf -counter-select 0 3 6
 
 echo "Mask node 0"
-echo "TESTING ./nc_perf fabric-loop-05.json -counter-mask 0"
-./nc_perf fabric-loop-05.json -counter-mask 0
-echo "TESTING ./nc_perf fabric-loop-05.json -counter-mask 0 1"
-./nc_perf fabric-loop-05.json -counter-mask 0 1
-echo "TESTING ./nc_perf fabric-loop-05.json -counter-mask 9 2 3"
-./nc_perf fabric-loop-05.json -counter-mask 9 2 3
-echo "TESTING ./nc_perf fabric-loop-05.json -counter-mask 0 9 3"
-./nc_perf fabric-loop-05.json -counter-mask 0 9 3
-echo "TESTING ./nc_perf fabric-loop-05.json -counter-mask 0 2 9"
-./nc_perf fabric-loop-05.json -counter-mask 0 2 9
+echo "TESTING ./nc_perf  -counter-mask 0"
+./nc_perf -counter-mask 0
+echo "TESTING ./nc_perf  -counter-mask 0 1"
+./nc_perf -counter-mask 0 1
+echo "TESTING ./nc_perf  -counter-mask 9 2 3"
+./nc_perf -counter-mask 9 2 3
+echo "TESTING ./nc_perf  -counter-mask 0 9 3"
+./nc_perf -counter-mask 0 9 3
+echo "TESTING ./nc_perf  -counter-mask 0 2 9"
+./nc_perf -counter-mask 0 2 9
 
 echo "Mask them - only node 0 - FIX" 
-./nc_perf fabric-loop-05.json -counter-mask 0 0 6
-./nc_perf fabric-loop-05.json -counter-mask 0 1 5
-./nc_perf fabric-loop-05.json -counter-mask 0 2 3
-./nc_perf fabric-loop-05.json -counter-mask 0 3 2
+./nc_perf -counter-mask 0 0 6
+./nc_perf -counter-mask 0 1 5
+./nc_perf -counter-mask 0 2 3
+./nc_perf -counter-mask 0 3 2
 
 echo "read counter - node 0"
-echo "TESTING ./nc_perf fabric-loop-05.json -counter-read 0 "
-./nc_perf fabric-loop-05.json -counter-read 0 
-echo "TESTING ./nc_perf fabric-loop-05.json -counter-read 0 9"
-./nc_perf fabric-loop-05.json -counter-read 0 9
-
-
+echo "TESTING ./nc_perf  -counter-read 0 "
+./nc_perf -counter-read 0 
+echo "TESTING ./nc_perf  -counter-read 0 9"
+./nc_perf -counter-read 0 9
