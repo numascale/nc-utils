3694d55b1bf866461899929327b8a00706ce3b6c
Daniel J Blueman <daniel@numascale-asia.com>
2013-11-14 17:09:35 +0800

    Implement class-based AML bytecode compiler

--- bootloader/dnc-aml.c
+++ bootloader/dnc-aml.c
@@ -28,43 +28,6 @@
 #include "dnc-commonlib.h"
 #include "dnc-bootloader.h"
 
-#define BUF_LIM		4096
-
-#define CHECKSUM	(uint8_t)0x00 /* Not checked */
-#define NAME		(uint8_t)0x08
-#define BYTE		(uint8_t)0x0a
-#define WORD		(uint8_t)0x0b
-#define DWORD		(uint8_t)0x0c
-#define QWORD		(uint8_t)0x0e
-#define SCOPE		(uint8_t)0x10
-#define BUFFER		(uint8_t)0x11
-#define PACKAGE		(uint8_t)0x12
-#define METHOD		(uint8_t)0x14
-#define RETURN		(uint8_t)0xa4
-#define ENDTAG		(uint8_t)0x79
-#define MARKER		(uint8_t)0xff
-#define EISAID		(uint16_t)0xd041
-#define DEVICE		(uint16_t)0x825b
-
-#define TypeMemory	0
-#define TypeIO		1
-#define TypeBus		2
-#define ResourceProducer 1
-#define ResourceConsumer 0
-#define Fixed		1
-#define NotFixed	0
-#define SubDecode	1
-#define PosDecode	0
-
-#define AML_ADDR_BITS	48
-#define NAME_MAX	16
-#define BLOCK_MAX	(1ULL << (64 - AML_ADDR_BITS))
-
-#define emit(p, x) do { \
-    *(typeof(x) *)(p) = (x); \
-    (p) += sizeof(x); \
-  } while (0)
-
 #define N16(x) (uint16_t)( \
     (((x) & 0xff00) >> 8) | \
     (((x) & 0x00ff) << 8))
@@ -83,235 +46,316 @@
     (((x) & 0x00ff000000000000ULL) >> 40) | \
     (((x) & 0xff00000000000000ULL) >> 56))
 
-typedef uint64_t ptrlen_t;
-typedef unsigned char aml;
-typedef ptrlen_t (*aml_func)(ptrlen_t);
+class Container {
+	static const int maxchildren = 12;
+	Container *children[maxchildren];
+	int nchildren;
+protected:
+	static const int buflen = 16384;
+	static const uint8_t BYTE = 0x0a;
+	static const uint8_t WORD = 0x0b;
+	static const uint8_t DWORD = 0x0c;
+	static const uint8_t QWORD = 0x0e;
+public:
+	static unsigned char *buf, *pos;
 
-static inline aml *PTR(ptrlen_t val)
-{
-	return (aml *)(uint32_t)(val & ((1ULL << AML_ADDR_BITS) - 1));
-}
+	enum ResourceUsage {ResourceConsumer, ResourceProducer};
+	enum MinType {MinNotFixed, MinFixed};
+	enum MaxType {MaxNotFixed, MaxFixed};
+	enum Decode {PosDecode, SubDecode};
+	enum Cacheability {Cacheable, Uncacheable};
+	enum MemAccess {ReadWrite};
 
-static inline int LEN(ptrlen_t val)
-{
-	return val >> AML_ADDR_BITS;
-}
+	Container(void): nchildren(0) {}
 
-static inline uint64_t ptrlen(aml *start, aml *end)
-{
-	uint64_t mask = (uint32_t)start;
-	assert(mask < (1ULL << AML_ADDR_BITS));
-	return mask | (uint64_t)(end - start) << AML_ADDR_BITS;
-}
-
-static inline void aml_str(aml **block, const char *str)
-{
-	strcpy(*(char **)block, str);
-	*block += strlen(str);
-}
-
-static void aml_length(aml **block, uint32_t len)
-{
-	if (len < 64) {
-		emit(*block, (uint8_t)len);
-		return;
+	~Container(void) {
+		while (nchildren) {
+			delete children[nchildren - 1];
+			nchildren--;
+		}
 	}
 
-	unsigned char *pkglen = *block;
-	/* First octet stores 4 LSbs */
-	emit(*block, (uint8_t)(len & 0xf));
-	len >>= 4;
-
-	while (len) {
-		emit(*block, (uint8_t)len);
-		len >>= 8;
+	void child(Container *c) {
+		children[nchildren++] = c;
+		assert(nchildren < maxchildren);
 	}
 
-	/* Compute and store additional octets in 7:6 of first octet */
-	uint8_t octets = *block - pkglen - 1;
-	assert(octets < 4);
-	*pkglen |= octets << 6;
-}
+	void emit(void) {} /* Overridden in inheriters */
 
-static void aml_name(aml **block, const char *name)
-{
-	emit(*block, NAME);
-	aml_str(block, name);
-}
-
-static void aml_eisaid(aml **block, const uint16_t id)
-{
-	emit(*block, DWORD);
-	emit(*block, EISAID);
-	emit(*block, N16(id));
-}
-
-static void aml_constant(aml **block, uint64_t val)
-{
-	if (val <= 1) {
-		emit(*block, (uint8_t)val);
-	} else if (val <= 0xff) {
-		emit(*block, BYTE);
-		emit(*block, (uint8_t)val);
-	} else if (val <= 0xffff) {
-		emit(*block, WORD);
-		emit(*block, (uint16_t)val);
-	} else if (val <= 0xffffffff) {
-		emit(*block, DWORD);
-		emit(*block, (uint32_t)val);
-	} else {
-		emit(*block, QWORD);
-		emit(*block, val);
+	void pack(const uint8_t val) {
+		*(uint8_t *)pos = val;
+		pos += sizeof(val);
+		assert(pos < buf + buflen);
 	}
-}
 
-static void aml_return(aml **block, uint64_t val)
-{
-	emit(*block, RETURN);
-	aml_constant(block, val);
-}
+	void pack(const uint16_t val) {
+		*(uint16_t *)pos = val;
+		pos += sizeof(val);
+		assert(pos < buf + buflen);
+	}
 
-static ptrlen_t aml_cba(const uint64_t addr)
-{
-	aml *block_start = (aml *)malloc(BLOCK_MAX);
-	assert(block_start);
-	aml *block = block_start;
-	aml_return(&block, addr);
-	return ptrlen(block_start, block);
-}
+	void pack(const uint32_t val) {
+		*(uint32_t *)pos = val;
+		pos += sizeof(val);
+		assert(pos < buf + buflen);
+	}
 
-static void aml_resource(aml **block, uint8_t type, bool mintype, bool maxtype, bool decode,
-                         uint16_t gran, uint16_t min, uint16_t max, uint16_t trans, uint16_t len)
-{
-	aml *pkg_start = *block;
-	emit(*block, BUFFER);
-	aml *pkglen = *block;
-	emit(*block, MARKER); /* PackageLength, updated later */
-	aml *buf_start = *block;
-	emit(*block, (uint8_t)0x0a); /* BufferSize */
-	aml *bufsize = *block;
-	emit(*block, (uint8_t)0); /* Buffer size byte, updated later */
-	emit(*block, (uint8_t)0x88);
-	emit(*block, (uint16_t)0x000d); /* Minimum length (13) */
-	emit(*block, type);
-	emit(*block, (uint8_t)((decode << 1) | (mintype << 2) | (maxtype << 3)));
-	emit(*block, (uint8_t)0); /* Type-specific flags; 0 for bus */
-	emit(*block, gran);
-	emit(*block, min);
-	emit(*block, max);
-	emit(*block, trans);
-	emit(*block, len);
-	emit(*block, ENDTAG);
+	void pack(const uint64_t val) {
+		*(uint64_t *)pos = val;
+		pos += sizeof(val);
+		assert(pos < buf + buflen);
+	}
 
-	/* Write length */
-	*pkglen = *block - pkg_start;
-	*bufsize = *block - buf_start - 1;
-	emit(*block, CHECKSUM);
-}
+	void pack(const char *str) {
+		strcpy((char *)pos, str);
+		pos += strlen(str);
+		assert(pos < buf + buflen);
+	}
 
-static void aml_method(aml **outer, const char *name, ptrlen_t block)
-{
-	aml *inner = PTR(block);
-	uint32_t len = LEN(block);
+	void pack_length(size_t len) {
+		if (len < 64) {
+			pack((uint8_t)len);
+			return;
+		}
 
-	emit(*outer, METHOD);
-	aml_length(outer, len + strlen(name) + 2);
-	strcpy(*(char **)outer, name);
-	*outer += strlen(name);
-	emit(*outer, (uint8_t)0); /* Field flags */
-	memcpy(*(char **)outer, inner, len);
-	*outer += len;
-	free(inner);
-}
+		unsigned char *pkglen = pos;
+		/* First octet stores 4 LSBs */
+		pack((uint8_t)(len & 0xf));
+		len >>= 4;
 
-static ptrlen_t aml_pci(uint16_t node)
-{
-	aml *block_start = (aml *)malloc(BLOCK_MAX);
-	assert(block_start);
-	aml *block = block_start;
-	/* Name (_HID, EisaId("PNP0A08")) */
-	aml_name(&block, "_HID");
-	aml_eisaid(&block, 0x0a08);
-	/* Name (_HID, EisaId("PNP0A03")) */
-	aml_name(&block, "_CID");
-	aml_eisaid(&block, 0x0a03);
-	/* Name (_ADR, 0x00000000) */
-	aml_name(&block, "_ADR");
-	aml_constant(&block, 0);
-	/* Name (_UID, 0) */
-	aml_name(&block, "_UID");
-	aml_constant(&block, 0);
-	/* Name (_BBN, 0x00) */
-	aml_name(&block, "_BBN");
-	aml_constant(&block, 0);
-	/* Name (_SEG, 0x01) */
-	aml_name(&block, "_SEG");
-	aml_constant(&block, node);
-	/* Name (_CRS, ResourceTemplate() {
-	     WordBusNumber(
-	       ResourceProducer, MinFixed, MaxFixed, PosDecode,
-	         0x0000, 0x0000, 0x00FF, 0x0000, 0x0100)}) */
-	aml_name(&block, "_CRS");
-	aml_resource(&block, TypeBus, Fixed, Fixed, PosDecode,
-	             0x0000, 0x0000, 0x00ff, 0x0000, 0x0100);
-	uint64_t addr = DNC_MCFG_BASE | ((uint64_t)node << 32);
-	aml_method(&block, "_CBA", aml_cba(addr));
-	return ptrlen(block_start, block);
-}
+		while (len) {
+			pack((uint8_t)len);
+			len >>= 8;
+		}
 
-static void aml_device(aml **outer, ptrlen_t block, const char *format, ...)
-{
-	emit(*outer, DEVICE);
-	char name[16];
+		/* Compute and store additional octets in 7:6 of first octet */
+		uint8_t octets = pos - pkglen - 1;
+		assert(octets < 4);
+		*pkglen |= octets << 6;
+	}
 
-	/* Emit formatted name */
-	va_list args;
-	va_start(args, format);
-	int chars = vsprintf(name, format, args);
-	va_end(args);
-	aml *inner = PTR(block);
-	uint32_t len = LEN(block);
-	aml_length(outer, len + chars + 2);
-	memcpy(*outer, name, chars);
-	*outer += chars;
-	memcpy(*outer, inner, len);
-	*outer += len;
-	free(inner);
-}
+	void pack_children(void) {
+		for (int i = 0; i < nchildren; i++)
+			children[i]->emit();
+	}
 
-static ptrlen_t aml_systembus(void)
-{
-	aml *block_start = (aml *)malloc(BLOCK_MAX);
-	assert(block_start);
-	aml *block = block_start;
+	uint32_t used(void) {
+		return pos - buf;
+	}
+};
 
-	for (uint16_t node = 1; node < dnc_node_count; node++)
-		aml_device(&block, aml_pci(node), "P%03d", node);
+class AML: public Container {
+public:
+	AML(void) {
+		/* Freed outside this class */
+		buf = (unsigned char *)malloc(buflen);
+		assert(buf);
+		pos = buf;
+	}
+};
 
-	return ptrlen(block_start, block);
-}
+class WordBusNumber: public Container {
+	static const uint8_t ENDTAG = 0x79;
+	static const uint8_t CHECKSUM = 0x00; /* Not checked */
+	static const uint8_t BUFFER = 0x11;
+	static const uint8_t MARKER = 0xff;
 
-static ptrlen_t aml_scope(const char *name, ptrlen_t block)
-{
-	aml *outer_start = (aml *)malloc(BLOCK_MAX);
-	assert(outer_start);
-	aml *outer = outer_start;
-	aml *inner = PTR(block);
-	uint32_t len = LEN(block);
-	emit(outer, SCOPE);
-	aml_length(&outer, len + strlen(name) + 2);
-	strcpy((char *)outer, name);
-	outer += strlen(name);
-	memcpy(outer, inner, len);
-	outer += len;
-	free(inner);
-	return ptrlen(outer_start, outer);
-}
+	const ResourceUsage resource;
+	const MinType mint;
+	const MaxType maxt;
+	const Decode decode;
+	const uint32_t gran, mina, maxa, trans, len;
+public:
+	WordBusNumber(const ResourceUsage _resource, const MinType _mint, const MaxType _maxt,
+	  const Decode _decode, const uint32_t _gran, const uint32_t _mina, const uint32_t _maxa,
+	  const uint32_t _trans, const uint32_t _len):
+	  resource(_resource), mint(_mint), maxt(_maxt), decode(_decode), gran(_gran),
+	  mina(_mina), maxa(_maxa), trans(_trans), len(_len) {}
+
+	void emit(void) {
+		unsigned char *pkg_start = pos;
+		pack(BUFFER);
+		unsigned char *pkglen = pos;
+		pack(MARKER); /* PackageLength, updated later */
+		unsigned char *buf_start = pos;
+		pack((uint8_t)0x0a); /* BufferSize */
+		unsigned char *bufsize = pos;
+		pack((uint8_t)0); /* Buffer size byte, updated later */
+		pack((uint8_t)0x88);
+		pack((uint16_t)0x000d); /* Minimum length (13) */
+		pack((uint8_t)resource);
+		pack((uint8_t)((decode << 1) | (mint << 2) | (maxt << 3)));
+		pack((uint8_t)0); /* Type-specific flags; 0 for bus */
+		pack(gran);
+		pack(mina);
+		pack(maxa);
+		pack(trans);
+		pack(len);
+		pack(ENDTAG);
+
+		/* Write length */
+		*pkglen = pos - pkg_start;
+		*bufsize = pos - buf_start - 1;
+		pack(CHECKSUM);
+	}
+};
+
+class Constant: public Container {
+	const uint64_t val;
+public:
+	Constant(const uint64_t _val): val(_val) {}
+
+	void emit(void) {
+		if (val <= 1) {
+			pack((uint8_t)val);
+			return;
+		}
+
+		if (val <= 0xff) {
+			pack(BYTE);
+			pack((uint8_t)val);
+		}
+
+		if (val <= 0xffff) {
+			pack(WORD);
+			pack((uint16_t)val);
+		}
+
+		if (val <= 0xffffffff) {
+			pack(DWORD);
+			pack((uint32_t)val);
+		}
+
+		pack(QWORD);
+		pack(val);
+	}
+};
+
+class Method: public Container {
+	static const uint8_t METHOD = 0x14;
+	const char *name;
+public:
+	enum Serialisation {NotSerialised, Serialised};
+
+	Method(const char *_name, const int, const Serialisation): name(_name) {}
+
+	void emit(void) {
+		pack(METHOD);
+		pack_length(strlen(name));
+		pack(name);
+		pack((uint8_t)0); /* Field flags */
+		pack_children();
+	}
+};
+
+class EisaId: public Container {
+	static const uint16_t EISAID = 0xd041;
+	const uint16_t id;
+public:
+	EisaId(const uint16_t _id): id(_id) {}
+
+	void emit(void) {
+		pack(DWORD);
+		pack(EISAID);
+		pack(N16(id));
+	}
+};
+
+class Name: public Container {
+	const char *name;
+public:
+	Name(const char *_name, Container *c): name(_name) {
+		child(c);
+	};
+
+	void emit(void) {
+		pack(name);
+	}
+};
+
+class Return: public Container {
+	static const uint8_t RETURN = 0xa4;
+public:
+	Return(Container *c) {
+		child(c);
+	}
+
+	void emit(void) {
+		pack(RETURN);
+	}
+};
+
+class Device: public Container {
+	static const uint16_t DEVICE = 0x825b;
+	static const uint16_t PNP0A08 = 0x0a08;
+	static const uint16_t PNP0A03 = 0x0a03;
+	const char *name;
+public:
+	Device(const char *_name, const int node): name(_name) {
+		child(new Name("_HID", new EisaId(PNP0A08)));
+		child(new Name("_CID", new EisaId(PNP0A03)));
+		child(new Name("_ADR", new Constant(0x00)));
+		child(new Name("_UID", new Constant(0x00)));
+		child(new Name("_BBN", new Constant(0x00)));
+		child(new Name("_SEG", new Constant(node)));
+		child(new Name("_CRS", new WordBusNumber(
+		  WordBusNumber::ResourceProducer,
+		  WordBusNumber::MinFixed,
+		  WordBusNumber::MaxFixed,
+		  WordBusNumber::PosDecode,
+		  0x0000,
+		  0x0000,
+		  0x00FF,
+		  0x0000,
+		  0x1000)));
+
+		Container *method = new Method("_CBA", 0, Method::NotSerialised);
+		const uint64_t config = DNC_MCFG_BASE | ((uint64_t)node << 32);
+		Container *passed = new Return(new Constant(config));
+		method->child(passed);
+		child(method);
+	}
+
+	void emit(void) {
+		pack(DEVICE);
+		pack_length(strlen(name));
+		pack(name);
+
+		pack_children();
+	}
+};
+
+class Scope: public Container {
+	static const uint8_t SCOPE = 0x10;
+	const char *name;
+public:
+	Scope(const char *_name): name(_name) {}
+
+	void emit(void) {
+		pack(SCOPE);
+		pack_length(strlen(name));
+		pack(name);
+	}
+};
+
+unsigned char *Container::buf = NULL, *Container::pos = NULL;
 
 unsigned char *remote_aml(uint32_t *len)
 {
-	ptrlen_t block = aml_scope("\\_SB_", aml_systembus());
-	*len = LEN(block);
-	return PTR(block);
+	AML sdst = AML();
+	Container *sb = new Scope("\\_SB_");
+
+	for (int node = 1; node < dnc_node_count; node++) {
+		char name[4];
+		snprintf(name, sizeof(name), "R%03X", node);
+
+		Container *device = new Device(name, node);
+		sb->child(device);
+	}
+
+	sdst.child(sb);
+
+	*len = sdst.used();
+	return sdst.buf;
 }
 
