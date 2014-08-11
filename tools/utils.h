1acbb6077ea1b9fdebb0e5d4d85344e88aacaa18
Daniel J Blueman <daniel@numascale.com>
2014-08-11 09:39:06 +0800

    Various updates

---
+++ tools/utils.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2008-2014 Numascale AS, support@numascale.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define roundup(x, n) (((x) + ((n) - 1)) & (~((n) - 1)))
+
+#define assertf(cond, format, args...) do { if (!(cond)) { \
+  fprintf(stderr, "Error: "); \
+  fprintf(stderr, format, ## args); \
+  fprintf(stderr, "\n"); \
+  _exit(1); \
+} } while (0)
+
+#define sysassertf(cond, format, args...) do { if (!(cond)) { \
+  fprintf(stderr, "Error: "); \
+  fprintf(stderr, format, ## args); \
+  fprintf(stderr, " with %s\n", strerror(errno)); \
+  _exit(1); \
+} } while (0)
+
+#define error(format, args...) do { \
+  fprintf(stderr, "Error: "); \
+  fprintf(stderr, format, ## args); \
+  fprintf(stderr, "\n"); \
+  _exit(1); \
+} while (0)
+
+#define syserror(format, args...) do { \
+  fprintf(stderr, "Error: "); \
+  fprintf(stderr, format, ## args); \
+  fprintf(stderr, " with %s\n", strerror(errno)); \
+  _exit(1); \
+} while (0)
+
+#define PLACE_VERBOSE 1
