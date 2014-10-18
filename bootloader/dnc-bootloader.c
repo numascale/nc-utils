b34f7c1af4c67d17eca719dcc2646d9d504ccfe8
Daniel J Blueman <daniel@numascale.com>
2014-10-18 20:40:00 +0800

    Use consistent notation

--- bootloader/dnc-bootloader.c
+++ bootloader/dnc-bootloader.c
@@ -317,7 +317,7 @@ static void install_e820_handler(void)
 	int_vecs[0x15] = (((uint32_t)asm_relocated) << 12) |
 	                 ((uint32_t)(&new_e820_handler_relocate - &asm_relocate_start));
 	printf("Persistent code relocated to %p:%p\n", asm_relocated, asm_relocated + relocate_size);
-	printf("Allocating ACPI tables at %p - %p\n", tables_relocated, tables_relocated + TABLE_AREA_SIZE);
+	printf("Allocating ACPI tables at %p:%p\n", tables_relocated, tables_relocated + TABLE_AREA_SIZE);
 	if (verbose > 0)
 		printf("__mem_end = %p, __stack_size = 0x%zx, sp() = 0x%x\n", __mem_end, __stack_size, sp());
 }
